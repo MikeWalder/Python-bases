c822abfc9fb50ee3fc2fe9555973b7f7b7ba6902
MikeWalder <mike_walder@hotmail.fr>
2022-05-31 09:52:03 +0200

    Script calculatrice V.2

--- projets/calculatrice/calculatrice.py
+++ projets/calculatrice/calculatrice.py
@@ -1,3 +1,22 @@
+def checkNumberInput(variable):
+    number = input(f"Entrez un entier {variable} :")
+    try:
+        # Convert into an integer
+        numberIntegered = int(number)
+        print("Vous avez entré l'entier",numberIntegered)
+        # print(type(number))
+        # print(type(numberIntegered))
+        return numberIntegered
+    except ValueError:
+        try:
+            # Convert into a float
+            numberFloated = float(number)
+            print("Le nombre est un flottant et vaut",numberFloated)
+            return checkNumberInput(variable)
+        except ValueError:
+            print("Le nombre est autre chose")
+            return checkNumberInput(variable)
+
 def selectNumbers(entry):
     try:
         number = int(input(f"Choisir {entry} : "))
@@ -6,35 +25,44 @@ def selectNumbers(entry):
     except ValueError:
         print("Entrée invalide !")
 
+
 print("------ CALCULATRICE V.2 ------")
 print("Choix de l'opération : ")
-print("1 : Addition \n2 : Soustraction \n3 : Multiplication \n4 : Division")
+print("1 : Addition \n2 : Soustraction \n3 : Multiplication \n4 : Division \n5 : Puissance")
 choice = input("Choix : ")
 
 if choice == "1":
-    print("Addition")
-    a = selectNumbers("a")
-    b = selectNumbers("b")
+    print("Choix : Addition")
+    a = checkNumberInput("a")
+    b = checkNumberInput("b")
     res = a + b 
-    print(f"Résultat : {res}")
+    print(f"Résultat de l'addition de {a} par {b} : {res}")
 elif choice == "2":
-    print("Soustraction")
-    a = selectNumbers("a")
-    b = selectNumbers("b")
+    print("Choix : Soustraction")
+    a = checkNumberInput("a")
+    b = checkNumberInput("b")
     res = a - b
-    print(f"Résultat : {res}")
+    print(f"Résultat de la soustraction de {a} par {b} : {res}")
 elif choice == "3":
-    print("Multiplication")
-    a = selectNumbers("a")
-    b = selectNumbers("b")
+    print("Choix : Multiplication")
+    a = checkNumberInput("a")
+    b = checkNumberInput("b")
     res = a * b
-    print(f"Résultat : {res}")
+    print(f"Résultat de la multiplication de {a} par {b} : {res}")
 elif choice == "4":
-    print("Division")
-    a = selectNumbers("a")
-    b = selectNumbers("b")
+    print("Choix : Division")
+    a = checkNumberInput("a")
+    b = checkNumberInput("b")
     res = a / b
-    print(f"Résultat : {res}")
+    print(f"Résultat de la division de {a} par {b} : {res}")
+elif choice == "5":
+    print("Choix : Puissance (a^b)")
+    a = checkNumberInput("a")
+    b = checkNumberInput("b")
+    res = a ** b
+    print(f"Résultat de {a} puissance {b} : {res}")
 else:
     print("Mauvais choix")
+
+
 
